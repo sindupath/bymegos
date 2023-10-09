7af5f559a57d715acdf1d25c0fc05555fab2701a
sindupath <sindupathofficial@gmail.com>
2023-10-09 17:02:50 +0530

    dark mode

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function bar(){
+    var bar =document.getElementById('menu');
+    bar.classList.toggle('menu');
+    
+}
+
+function dark(){
+    
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+   
+}
